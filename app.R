607c774a646e13cef6b8516227da3dbd8fd33f86
augustgoodlund <164281323+augustgoodlund@users.noreply.github.com>
2024-05-30 16:11:53 -0700

    Added widgets to app

--- app.R
+++ app.R
@@ -1,44 +1,50 @@
 library(shiny)
-runApp("myapp")
+library(bslib)
+library(ggplot2)
+library(dplyr)
 data=iris
 
+
 ui <- page_sidebar(
-  title = "censusVis",
+  title = "Irises",
+  "Choose Variables",
   sidebar = sidebar(
-    helpText(
-      "Select data from 3 species of iris."
-    ),
-    selectInput(
-      "var",
-      label = "Choose a variable to display",
-      choices = c("Petal length",
-                  "Petal width",
-                  "Sepal length",
-                  "Sepal width",
-                  "Species"),
-      selected = "Petal length"
-    ),
-    sliderInput(
-      "range",
-      label = "Range(cm):",
-      min = 0, max = 10, value = c(0, 10)
-    )
+    position="right",
+    card(helpText(
+      "Look at me, I'm up here!"
+    )),
+    card(card_header("What flower do you like best?"),
+         textInput("text", label = NULL, value = "Enter species..."),
+         actionButton("action", label = "Submit")),
   ),
-  textOutput("selected_var"),
-  textOutput("min_max") 
+  selectInput(
+    inputId = "y",
+    label = "Y-axis",
+    choices = c("Sepal.Length", "Sepal.Width",  "Petal.Length", "Petal.Width", "Species"),
+    selected = "Petal.Width"
+  ),
+  selectInput(
+    inputId = "x",
+    label = "X-axis",
+    choices = c("Sepal.Length", "Sepal.Width",  "Petal.Length", "Petal.Width", "Species"),
+    selected = "Species"
+  ),
+  card(plotOutput(outputId = "scatterplot"))
 )
 
-server <- function(input, output) {
+
+
   
-  output$selected_var <- renderText({
-    paste("You have selected", input$var)
+
+
+
+
+
+server <- function(input, output, session) {
+  output$scatterplot <- renderPlot({
+    ggplot(data = iris, aes_string(x = input$x, y = input$y)) +
+      geom_point()
   })
-  
-  output$min_max <- renderText({
-    paste("You have chosen a range that goes from",
-          input$range[1], "to", input$range[2])
-  })
-  
 }
 
 shinyApp(ui, server)
